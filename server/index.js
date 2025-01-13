e4383535f83ca7451827bc330b15e6ee653fd35c
minb__m <kmb3322@naver.com>
2025-01-13 15:07:30 +0900

    cors2차

--- server/index.js
+++ server/index.js
@@ -25,7 +25,10 @@ const upload = multer({
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
 const additionalOrigins = [
   'http://localhost:5173', 
-  'https://51ef-2001-2d8-6a87-cd2e-8450-a2e1-9d1a-764e.ngrok-free.app'
+  'https://51ef-2001-2d8-6a87-cd2e-8450-a2e1-9d1a-764e.ngrok-free.app',
+  'https://fantastic-alcohol.vercel.app', 
+  'https://soju.monster'
+
 ];
 
 // CORS 설정: FRONTEND_URL과 추가 도메인을 허용
