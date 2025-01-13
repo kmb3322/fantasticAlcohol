c8128e2b6babcaa3f72a7879d6858fb3c9778490
minb__m <kmb3322@naver.com>
2025-01-13 19:27:21 +0900

    제발 소켓좀

--- src/socket.ts
+++ src/socket.ts
@@ -2,5 +2,5 @@
 import { io } from 'socket.io-client';
 
 // 서버 주소 맞게 설정
-const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
+const backendUrl = import.meta.env.VITE_BACKEND_URL || 'https://thisis.sojubackend.monster';
 export const socket = io(backendUrl);
