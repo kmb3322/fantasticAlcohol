6a5d9e60d465d94850bc2acc29c9974eaacefc45
minb__m <kmb3322@naver.com>
2025-01-14 14:15:52 +0900

    ㅋㅋ

--- src/socket.ts
+++ src/socket.ts
@@ -2,7 +2,7 @@
 import { io } from 'socket.io-client';
 
 // 서버 주소 맞게 설정
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'https://thisis.sojubackend.monster';
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'https://sojubackend.monster';
 export const socket = io(BACKEND_URL, {
     withCredentials: true,
     extraHeaders: {
